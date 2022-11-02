57023012908774c015ddb4ae6992532f312aec2f
felipevillajr <felipevilla2010@live.com>
2022-11-02 19:38:59 -0400

    maping out home page with html

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Hero from "./Components/herobanner/herobanner";
 
 function App() {
-  const [word, setWord] = useState();
+  // const [word, setWord] = useState();
 
-  useEffect(() => {
-    console.log(word);
-  });
+  // useEffect(() => {
+  //   console.log(word);
+  // });
 
-  const hChange = (e) => {
-    setWord({ ...word, [e.target.name]: e.target.value });
-  };
-  x;
+  // const hChange = (e) => {
+  //   setWord({ ...word, [e.target.name]: e.target.value });
+  // };
+  // x;
 
-  const reset = (e) => {
-    setWord("");
-  };
+  // const reset = (e) => {
+  //   setWord("");
+  // };
 
   return (
     <div>
+      <Header />
+      <Hero />
       <div>
         <p>Broklyn Tattoo and Piercings</p>
       </div>
-      <input
+      {/* <input
         type="text"
         name="input"
         id="name"
@@ -31,7 +36,8 @@ function App() {
         onChange={hChange}
         onSubmit={reset}
       />
-      <button id="name" onClick={() => reset}></button>
+      <button id="name" onClick={() => reset}></button> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/herobanner/herobanner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Carousel from "../Carousel/Carousel";
+
+export default function herobanner() {
+  return (
+    <div className="herobanner">
+      <Carousel />
+      <div>Herobanner</div>
+    </div>
+  );
+}
